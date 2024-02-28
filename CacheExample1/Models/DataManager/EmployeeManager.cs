69cfb052cb4398fc8c49a4cfb37964c433958cc0
jayavardhan5555 <89379481+jayavardhan5555@users.noreply.github.com>
2024-02-28 23:02:38 +0530

    Add project files.

---
+++ CacheExample1/Models/DataManager/EmployeeManager.cs
@@ -0,0 +1,42 @@
+﻿using CacheExample1.Models.Repository;
+
+namespace CacheExample1.Models.DataManager
+{
+    public class EmployeeManager: IDataRepository<Employee>
+    {
+        readonly EmployeeContext _employeeContext;
+
+        public EmployeeManager(EmployeeContext employeeContext)
+        {
+            _employeeContext = employeeContext;
+        }
+        public IEnumerable<Employee> GetAll()
+        {
+            return _employeeContext.Employees.ToList();
+        }
+        public Employee Get(long id)
+        {
+            return _employeeContext.Employees
+                  .FirstOrDefault(e => e.EmployeeId == id);
+        }
+        public void Add(Employee entity)
+        {
+            _employeeContext.Employees.Add(entity);
+            _employeeContext.SaveChanges();
+        }
+        public void Update(Employee employee, Employee entity)
+        {
+            employee.FirstName = entity.FirstName;
+            employee.LastName = entity.LastName;
+            employee.Email = entity.Email;
+            employee.DateOfBirth = entity.DateOfBirth;
+            employee.PhoneNumber = entity.PhoneNumber;
+            _employeeContext.SaveChanges();
+        }
+        public void Delete(Employee employee)
+        {
+            _employeeContext.Employees.Remove(employee);
+            _employeeContext.SaveChanges();
+        }
+    }
+}
